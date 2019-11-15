0699adf00706ed36a2e635281a2afb353fc1838f
Gowri Harish <gharish@Purdue.edu>
2019-11-15 17:35:36 -0500

    Passenger Write to File

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,4 +1,7 @@
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Passenger - CS 180 Project 5
  *
@@ -16,7 +19,7 @@ public class Passenger implements Serializable {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.fullName = firstName.substring(0, 1) + " " + lastName;
+        this.fullName = firstName.substring(0, 1).toUpperCase() + ". " + lastName.toUpperCase();
         this.boardingPass = null;
     }
 
@@ -42,5 +45,39 @@ public class Passenger implements Serializable {
 
     public int getAge() {
         return age;
+    }
+
+    public void writeToFile(String airplaneName) throws IOException {
+
+        //reading and updating arraylist
+        ArrayList<String> temp = new ArrayList<>();
+        String toAdd = this.fullName + ", " + this.age;
+        File file = new File("reservations.txt");
+        Scanner in = new Scanner(file);
+        while (in.hasNextLine()) {
+            temp.add(in.nextLine());
+        }
+        int findFirstAirplaneName = temp.indexOf(airplaneName);
+        int placeToAdd = temp.lastIndexOf("---------------------" + airplaneName);
+        temp.add(placeToAdd, toAdd);
+        temp.add(placeToAdd, "---------------------" + airplaneName);
+        String passNum = temp.get(temp.indexOf(airplaneName) + 1).split("/")[0];
+        String totalPassengers = temp.get(temp.indexOf(airplaneName) + 1).split("/")[1];
+        int toUpdatePassNum = Integer.parseInt(passNum);
+        toUpdatePassNum++;
+        temp.set(findFirstAirplaneName + 1, Integer.toString(toUpdatePassNum) + "/" + totalPassengers);
+        in.close();
+
+        //writing arraylist into file
+        FileOutputStream fos = new FileOutputStream(file);
+        PrintWriter pw = new PrintWriter(fos);
+        BufferedWriter bw = new BufferedWriter(pw);
+        for (String s : temp) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
+        pw.close();
+        fos.close();
     }
 }
--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
+
 public class hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Passenger p = new Passenger("Hello", "World", 20);
+        p.writeToFile("ALASKA");
     }
 }
