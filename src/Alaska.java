cf07570abfea097396dac8e8545156b39c101ab0
Aryan Wadhwani <wadhwani@purdue.edu>
2019-11-15 15:42:38 -0500

    Added Alaska, Delta and Southwest Updated Southwest

---
+++ src/Alaska.java
@@ -0,0 +1,19 @@
+/**
+ * Alaska - CS 180 Project 5
+ *
+ * @author Aryan Wadhwani, Gowri Harish, CS 18000
+ * @version 15th November 2019
+ */
+
+public class Alaska extends Airline {
+
+    public Alaska() {
+        super("Alaska", "Alaska Airlines is proud to serve the strong and knowledgeable " +
+                "Boilermakers from Purdue University." +
+                "\nWe primarily fly westward, and often have stops in Alaska and California." +
+                "\nWe have first class amenities, even in coach class." +
+                "\nWe provide fun snacks, such as pretzels and goldfish." +
+                "\nWe also have comfortable seats, and free WiFi." +
+                "\nWe hope you choose Alaska Airlines for your next itinerary!");
+    }
+}
