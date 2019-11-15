889ca2d3cb6194b61064dfb93aa012c6da106124
Gowri Harish <gharish@Purdue.edu>
2019-11-15 13:41:44 -0500

    Airline ver 1

---
+++ src/Airline.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public abstract class Airline {
+    String description;
+    String airplaneName;
+    int numPassengers;
+    int maxPassengers;
+    Gate gate;
+    ArrayList<Passenger> passenger;
+
+    public Airline(String description, String airplaneName, int numPassengers, int maxPassengers, Gate gate, ArrayList<Passengers> passengers) {
+        this.description = description;
+        this.airplaneName = airplaneName;
+        this.numPassengers = numPassengers;
+        this.maxPassengers = maxPassengers;
+        this.gate = gate;
+        this.passenger = passenger;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAirplaneName() {
+        return airplaneName;
+    }
+
+    public void setAirplaneName(String airplaneName) {
+        this.airplaneName = airplaneName;
+    }
+
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+
+    public void setNumPassengers(int numPassengers) {
+        this.numPassengers = numPassengers;
+    }
+
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    public void setMaxPassengers(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+
+    public Gate getGate() {
+        return gate;
+    }
+
+    public void setGate(Gate gate) {
+        this.gate = gate;
+    }
+
+    public ArrayList<Passenger> getPassenger() {
+        return passenger;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passenger) {
+        this.passenger = passenger;
+    }
+
+
+}
+
