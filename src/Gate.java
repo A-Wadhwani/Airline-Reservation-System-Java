476db754842ceb4d0fbf1dcaddfed6db2fc98229
unknown <wadhwani@purdue.edu>
2019-11-15 13:42:04 -0500

    Added Gate

---
+++ src/Gate.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+/**
+ * Gate - CS 180 Project 5
+ *
+ * @author Aryan Wadhwani, Gowri Harish, CS 18000
+ * @version Date Month 2019
+ */
+public class Gate {
+    private char gateLetter;
+    private int gateNumber;
+
+    public Gate() {
+        Random random = new Random();
+        this.gateLetter = new char[]{'A', 'B', 'C'}[random.nextInt(3)];
+        this.gateNumber = random.nextInt(18) + 1;
+    }
+
+    public String getGate(){
+        return String.format("%s%02d",gateLetter,gateNumber);
+    }
+
+    public Gate(char gateLetter, char gateNumber) {
+        this.gateLetter = gateLetter;
+        this.gateNumber = gateNumber;
+    }
+}
--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Gate gate = new Gate();
+        System.out.println(gate.getGate());
     }
 }
