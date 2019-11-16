2e0f62267476819b49cdb282b1f4fffe8d2b370a
Aryan Wadhwani <wadhwani@purdue.edu>
2019-11-16 17:22:42 -0500

    Did more Server Stuff

--- src/hello.java
+++ src/hello.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class hello {
 
+
     public static void main(String[] args) throws Exception {
-        Delta delta = new Delta();
-        System.out.println(delta.getPassengerDetails().toString());
+        Frame f1 = new Frame();
+        f1.setSize(500,500);
+        hello1 test = new hello1();
+        f1.add(test.showGUI());
+        f1.setVisible(true);
+        while(true) {
+            System.out.println(test.response());
+        }
     }
 
 }
