3e452c730cd35a70a1dbd07b622b69d45cd18529
Gowri Harish <gharish@Purdue.edu>
2019-11-15 21:35:45 -0500

    get hostname and port name

--- src/hello.java
+++ src/hello.java
@@ -1,19 +1,25 @@
-import java.io.IOException;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class hello {
-    public static void main(String[] args) throws Exception {
-        Airline airline = new Southwest();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your details: ");
-        String firstName = sc.next();
-        String lastName = sc.next();
-        int age = sc.nextInt();
-        Passenger p = new Passenger(firstName, lastName, age);
-        airline.addPassengers(p);
-        airline.UpdatePassengerDetails();
-        for (String passenger : airline.getPassengerDetails()) {
-            System.out.println(passenger);
-        }
+
+    public static String getHostnameAndPort(String message, String title) {
+        ImageIcon icon = new ImageIcon("JavaCup.png");
+        return (String) JOptionPane.showInputDialog(null, message, title, JOptionPane.OK_CANCEL_OPTION, icon,
+                null, "");
     }
+
+    public static void goToBookFlight() {
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String hostname = getHostnameAndPort("What is the hostname you's like to connect to?",
+                "Hostname?"); //hostname cannot be null
+        String port = getHostnameAndPort("What is the port you'd like to connect to?",
+                "Port?");
+        goToBookFlight();
+    }
+
 }
